d985d39464a2abb3753e237ce3fe57d93a18f985
Bertrand31 <bertrandjun@gmail.com>
2020-02-28 22:15:53 +0100

    Improve Line Sanitizing function

--- src/main/scala/cagire/LineSanitizing.scala
+++ src/main/scala/cagire/LineSanitizing.scala
@@ -4,8 +4,7 @@ object LineSanitizing {
 
   def lineToWords: String => Array[String] =
     _
-      .split("\\|| |/|\\[|\\]|\\{|\\}|\\\\|\\(|\\)|=|,")
-      .map(_.filter(_.isLetter))
+      .split("\\P{L}+") // Split on everything that isn't a unicode letter
       .filterNot(_.isEmpty)
       .map(_.toLowerCase)
 }
